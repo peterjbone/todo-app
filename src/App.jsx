e4d3cd7e956c168e344ca59b7efdd80968cd2703
peterjbone <peterjbone41@gmail.com>
2024-10-02 10:45:23 -0500

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,31 @@ function App() {
 	return (
 		<div className={styles.App}>
 			<h1>Todo app list</h1>
+
+			<div className={styles.todoWrapper}>
+				<div className={styles.inputsGroup}>
+					<div className={styles.inputItem}>
+						<label htmlFor="title">Title</label>
+						<input
+							id="title"
+							type="text"
+							placeholder="What is the task title?"
+						/>
+					</div>
+					<div className={styles.inputItem}>
+						<label htmlFor="description">Description</label>
+						<input
+							id="description"
+							type="text"
+							placeholder="What is the task description?"
+						/>
+					</div>
+					<div className={styles.inputItem}>
+						<label htmlFor="title">Title</label>
+						<input id="title" type="text" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
