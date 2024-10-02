2aa01878d7a8434e168b6ee8b849f60357f54811
peterjbone <peterjbone41@gmail.com>
2024-10-02 10:29:35 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import styles from "./App.module.css";
+
+function App() {
+	return (
+		<div className={styles.App}>
+			<h1>Todo app list</h1>
+		</div>
+	);
+}
+
+export default App;
