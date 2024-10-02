e738cc1dddd22bf55e67a07c8126e69a52eb6f5a
peterjbone <peterjbone41@gmail.com>
2024-10-02 11:17:48 -0500

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,35 @@ function App() {
 			<h1>Todo app list</h1>
 
 			<div className={styles.todoWrapper}>
+				{/* Inputs section */}
 				<div className={styles.inputsGroup}>
 					<div className={styles.inputItem}>
 						<label htmlFor="title">Title</label>
-						<input
-							id="title"
-							type="text"
-							placeholder="What is the task title?"
-						/>
+						<input id="title" type="text" placeholder="What is the task title?" />
 					</div>
 					<div className={styles.inputItem}>
 						<label htmlFor="description">Description</label>
-						<input
-							id="description"
-							type="text"
-							placeholder="What is the task description?"
-						/>
+						<input id="description" type="text" placeholder="What is the task description?" />
 					</div>
 					<div className={styles.inputItem}>
-						<label htmlFor="title">Title</label>
-						<input id="title" type="text" />
+						<button type="button" className={styles.primaryBtn}>
+							Add
+						</button>
+					</div>
+				</div>
+				{/* Inputs section */}
+				<div className={styles.buttonsGroup}>
+					<button type="button">Todo</button>
+					<button type="button">Completed</button>
+				</div>
+				{/* Tasks list */}
+				<div className={styles.taskList}>
+					<div className={styles.taskListItem}>
+						<b>Task 1</b>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio alias ut quae
+							voluptates corrupti.
+						</p>
 					</div>
 				</div>
 			</div>
