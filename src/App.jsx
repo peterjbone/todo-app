45908bd05c82ed17d64ef7acf409830270f858ca
peterjbone <peterjbone41@gmail.com>
2024-10-02 22:16:45 -0500

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import Input from "./components/Input/Input.jsx";
-import Button from "./components/Button/Button.jsx";
+//import Button from "./components/Button/Button.jsx";
 
 function App() {
+	const [isSelected, setIsSelected] = useState(null);
+
+	const handleClick = (e) => {
+		const { innerText } = e.target;
+		if (innerText === "Todo") setIsSelected("Todo");
+		if (innerText === "Completed") setIsSelected("Completed");
+	};
+
 	return (
 		<div className={styles.App}>
 			<h1>Todo app list</h1>
@@ -22,22 +30,32 @@ function App() {
 						/>
 					</div>
 					<div className={styles.inputItem}>
-						<Button text="Add" />
+						<button type="button">Add</button>
 					</div>
 				</div>
 				<hr />
 				{/* buttons section */}
 				<div className={styles.buttonsGroup}>
-					<button type="button">Todo</button>
-					<button type="button">Completed</button>
+					<button
+						type="button"
+						data-btn-selected={isSelected === "Todo" && "active"}
+						onClick={handleClick}>
+						Todo
+					</button>
+					<button
+						type="button"
+						data-btn-selected={isSelected === "Completed" && "active"}
+						onClick={handleClick}>
+						Completed
+					</button>
 				</div>
 				{/* Tasks list */}
 				<div className={styles.taskList}>
 					<div className={styles.taskListItem}>
 						<b>Task 1</b>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio alias ut quae
-							voluptates corrupti.
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+							distinctio alias ut quae voluptates corrupti.
 						</p>
 					</div>
 				</div>
